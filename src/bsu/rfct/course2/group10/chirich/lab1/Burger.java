b807c4a4161ee1086c51ba8c41eb94fecf6d5f46
Verona45 <chirrrkaaa@gmail.com>
2024-12-23 19:02:20 +0300

    Add files via upload

---
+++ src/bsu/rfct/course2/group10/chirich/lab1/Burger.java
@@ -0,0 +1,31 @@
+package bsu.rfct.course2.group10.chirich.lab1;
+
+public class Burger  extends Food{
+    private String size;
+    public Burger(String size) {
+        super("Бургер");
+        this.size = size;
+    }
+    public void consume() {
+        System.out.println(this + " съедено");
+    }
+    // Селектор для доступа к полю данных РАЗМЕР
+    public String getSize() {
+        return size;
+    }
+    // Модификатор для изменения поля данных РАЗМЕР
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public boolean equals(Object arg0) {
+        if (super.equals(arg0)) { // Шаг 1
+            if (!(arg0 instanceof Burger)) return false; // Шаг 2
+            return size.equals(((Burger)arg0).size); // Шаг 3
+        } else
+            return false;
+    }
+    public String toString() {
+        return super.toString() + " размера '" + size.toUpperCase() + "'"; //функция, выз.родительский конструктор(объект)
+    }
+}
+
---
+++ src/bsu/rfct/course2/group10/chirich/lab1/Consumable.java
@@ -0,0 +1,6 @@
+package bsu.rfct.course2.group10.chirich.lab1;
+
+public interface Consumable {
+    public abstract void consume();
+}
+
